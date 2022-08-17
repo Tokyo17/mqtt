cb341d43516b21f6d0880a7b38e30a9ad71067e8
mRezaMustofa <reza.mustofa@ist.id>
2022-08-17 16:39:01 +0700

    first commit

--- mqtt.js
+++ mqtt.js
@@ -3,9 +3,9 @@ const app=express()
 const port=8080
 app.use(express.json()) 
 
-// const mqtt=require("mqtt")
-// const topik="justcoba177"
-// const client=mqtt.connect("mqtt://test.mosquitto.org",{clientId:topik})
+const mqtt=require("mqtt")
+const topik="justcoba177"
+const client=mqtt.connect("mqtt://test.mosquitto.org",{clientId:topik})
 
 app.post("/",(req,res)=>{
     if(req.body.message=="1"){
@@ -15,21 +15,21 @@ app.post("/",(req,res)=>{
     }else{
         res.send("Wrong Message") 
     }
-    // client.publish(req.body.topic,req.body.message)
+    client.publish(req.body.topic,req.body.message)
 })
 
 
-// client.on('connect', function() {
-//   publish(topik,"0")
-// });
+client.on('connect', function() {
+  publish(topik,"0")
+});
 
-// const publish=(topic,message)=>{
+const publish=(topic,message)=>{
    
-//     if(client.connected){
-//         console.log(`publish to ${topic} : `,message)    
-//         client.publish(topic,message)
-//     }
-// }
+    if(client.connected){
+        console.log(`publish to ${topic} : `,message)    
+        client.publish(topic,message)
+    }
+}
 
 app.listen(port,()=>{
     console.log(`app started on ${port}`)
